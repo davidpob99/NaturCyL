3153811d68069ec6bf6cd61484f7221ecde5e650
David Población <david.poblacion@alumnos.uva.es>
2018-12-25 12:23:56 +0100

    Añadido OSM

--- app/src/main/java/es/jcyl/datosabiertos/apps/naturcyl/MainActivity.java
+++ app/src/main/java/es/jcyl/datosabiertos/apps/naturcyl/MainActivity.java
@@ -1,3 +1,24 @@
+/*
+ * Aplicación para consultar los espacios abiertos de la comunidad
+ * autónoma de Castilla y León, así como sus equipamientos y
+ * posibilidades.
+ *
+ * Copyright (C) 2018  David Población Criado
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package es.jcyl.datosabiertos.apps.naturcyl;
 
 import android.os.Bundle;
@@ -5,9 +26,9 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
